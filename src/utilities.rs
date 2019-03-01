364a9a05606f9f4ee3e6bcb71015af8c00a53adf
Graham Wihlidal <graham@wihlidal.ca>
2019-03-01 13:32:28 -0700

    Implemented texture format info and layout calculations

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 
+use crate::process::schema;
 use filebuffer::FileBuffer;
 use std::env;
 use std::fs::File;
@@ -250,5 +251,242 @@ pub fn exe_path() -> io::Result<std::path::PathBuf> {
     )))]
     {
         std::env::current_exe()
+    }
+}
+
+// TODO: Move to a better place, possibly a new crate
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub struct TextureFormatInfo {
+    pub block_width: u32,
+    pub block_height: u32,
+    pub block_bits: u32,
+    pub red_bits: u32,
+    pub green_bits: u32,
+    pub blue_bits: u32,
+    pub alpha_bits: u32,
+    pub depth_bits: u32,
+    pub stencil_bits: u32,
+    pub padding_bits: u32,
+    pub exponent_bits: u32,
+}
+
+pub fn get_texture_format_info(format: schema::TextureFormat) -> TextureFormatInfo {
+    use schema::TextureFormat;
+
+    let mut info: TextureFormatInfo = Default::default();
+
+    match format {
+        TextureFormat::R32G32B32A32_FLOAT
+        | TextureFormat::R32G32B32A32_UINT
+        | TextureFormat::R32G32B32A32_SINT => {
+            info.red_bits = 32;
+            info.green_bits = 32;
+            info.blue_bits = 32;
+            info.alpha_bits = 32;
+        }
+        TextureFormat::R32G32B32_FLOAT
+        | TextureFormat::R32G32B32_UINT
+        | TextureFormat::R32G32B32_SINT => {
+            info.red_bits = 32;
+            info.green_bits = 32;
+            info.blue_bits = 32;
+        }
+        TextureFormat::R16G16B16A16_FLOAT
+        | TextureFormat::R16G16B16A16_UNORM
+        | TextureFormat::R16G16B16A16_UINT
+        | TextureFormat::R16G16B16A16_SNORM
+        | TextureFormat::R16G16B16A16_SINT => {
+            info.red_bits = 16;
+            info.green_bits = 16;
+            info.blue_bits = 16;
+            info.alpha_bits = 16;
+        }
+        TextureFormat::R32G32_FLOAT | TextureFormat::R32G32_UINT | TextureFormat::R32G32_SINT => {
+            info.red_bits = 32;
+            info.green_bits = 32;
+        }
+        TextureFormat::D32_FLOAT_S8_UINT => {
+            info.depth_bits = 32;
+            info.stencil_bits = 8;
+            info.padding_bits = 24;
+        }
+        TextureFormat::R10G10B10A2_UNORM | TextureFormat::R10G10B10A2_UINT => {
+            info.red_bits = 10;
+            info.green_bits = 10;
+            info.blue_bits = 10;
+            info.alpha_bits = 2;
+        }
+        TextureFormat::R11G11B10_FLOAT => {
+            info.red_bits = 11;
+            info.green_bits = 11;
+            info.blue_bits = 10;
+        }
+        TextureFormat::R8G8B8_UNORM | TextureFormat::R8G8B8_SRGB => {
+            info.red_bits = 8;
+            info.green_bits = 8;
+            info.blue_bits = 8;
+        }
+        TextureFormat::R8G8B8A8_UNORM
+        | TextureFormat::R8G8B8A8_UINT
+        | TextureFormat::R8G8B8A8_SNORM
+        | TextureFormat::R8G8B8A8_SINT => {
+            info.red_bits = 8;
+            info.green_bits = 8;
+            info.blue_bits = 8;
+            info.alpha_bits = 8;
+        }
+        TextureFormat::R16G16_FLOAT
+        | TextureFormat::R16G16_UNORM
+        | TextureFormat::R16G16_UINT
+        | TextureFormat::R16G16_SNORM
+        | TextureFormat::R16G16_SINT => {
+            info.red_bits = 16;
+            info.green_bits = 16;
+        }
+        TextureFormat::D32_FLOAT => {
+            info.depth_bits = 32;
+        }
+        TextureFormat::R32_FLOAT | TextureFormat::R32_UINT | TextureFormat::R32_SINT => {
+            info.red_bits = 32;
+        }
+        TextureFormat::D24_UNORM_S8_UINT => {
+            info.depth_bits = 24;
+            info.stencil_bits = 8;
+        }
+        TextureFormat::R8G8_UNORM
+        | TextureFormat::R8G8_UINT
+        | TextureFormat::R8G8_SNORM
+        | TextureFormat::R8G8_SINT => {
+            info.red_bits = 8;
+            info.green_bits = 8;
+        }
+        TextureFormat::D16_UNORM => {
+            info.depth_bits = 16;
+        }
+        TextureFormat::R16_FLOAT
+        | TextureFormat::R16_UNORM
+        | TextureFormat::R16_UINT
+        | TextureFormat::R16_SNORM
+        | TextureFormat::R16_SINT => {
+            info.red_bits = 16;
+        }
+        TextureFormat::R8_UNORM
+        | TextureFormat::R8_UINT
+        | TextureFormat::R8_SNORM
+        | TextureFormat::R8_SINT => {
+            info.red_bits = 8;
+        }
+        TextureFormat::R9G9B9E5_FLOAT => {
+            info.red_bits = 9;
+            info.green_bits = 9;
+            info.blue_bits = 9;
+            info.exponent_bits = 5;
+        }
+        TextureFormat::R5G6B5_UNORM => {
+            info.red_bits = 5;
+            info.green_bits = 6;
+            info.blue_bits = 5;
+        }
+        TextureFormat::R5G5B5A1_UNORM => {
+            info.red_bits = 5;
+            info.green_bits = 6;
+            info.blue_bits = 5;
+            info.alpha_bits = 1;
+        }
+        TextureFormat::B8G8R8A8_UNORM | TextureFormat::B8G8R8A8_SRGB => {
+            info.red_bits = 8;
+            info.green_bits = 8;
+            info.blue_bits = 8;
+            info.alpha_bits = 8;
+        }
+        TextureFormat::BC1_UNORM
+        | TextureFormat::BC1A_UNORM
+        | TextureFormat::BC2_UNORM
+        | TextureFormat::BC3_UNORM
+        | TextureFormat::BC4_UNORM
+        | TextureFormat::BC4_SNORM
+        | TextureFormat::BC5_UNORM
+        | TextureFormat::BC5_SNORM
+        | TextureFormat::BC1_SRGB
+        | TextureFormat::BC1A_SRGB
+        | TextureFormat::BC2_SRGB
+        | TextureFormat::BC3_SRGB
+        | TextureFormat::BC6U_FLOAT
+        | TextureFormat::BC6S_FLOAT
+        | TextureFormat::BC7_UNORM
+        | TextureFormat::BC7_SRGB => {
+            // Ignore compressed formats
+        }
+        _ => {
+            unimplemented!();
+        }
+    }
+
+    // Calculate block size based on total bit count
+    info.block_bits += info.red_bits;
+    info.block_bits += info.green_bits;
+    info.block_bits += info.blue_bits;
+    info.block_bits += info.alpha_bits;
+    info.block_bits += info.depth_bits;
+    info.block_bits += info.stencil_bits;
+    info.block_bits += info.padding_bits;
+    info.block_bits += info.exponent_bits;
+
+    // A block size of zero is a compressed format
+    match info.block_bits {
+        0 => match format {
+            TextureFormat::BC1_UNORM
+            | TextureFormat::BC1_SRGB
+            | TextureFormat::BC1A_UNORM
+            | TextureFormat::BC1A_SRGB
+            | TextureFormat::BC4_UNORM => {
+                info.block_bits = 64;
+                info.block_width = 4;
+                info.block_height = 4;
+            }
+            TextureFormat::BC2_UNORM
+            | TextureFormat::BC2_SRGB
+            | TextureFormat::BC3_UNORM
+            | TextureFormat::BC3_SRGB
+            | TextureFormat::BC5_UNORM
+            | TextureFormat::BC6U_FLOAT
+            | TextureFormat::BC6S_FLOAT
+            | TextureFormat::BC7_UNORM
+            | TextureFormat::BC7_SRGB => {
+                info.block_bits = 128;
+                info.block_width = 4;
+                info.block_height = 4;
+            }
+            _ => {
+                unimplemented!();
+            }
+        },
+        _ => {
+            info.block_width = 1;
+            info.block_height = 1;
+        }
+    }
+
+    info
+}
+
+pub struct TextureLayoutInfo {
+    pub pitch: u32,
+    pub slice_pitch: u32,
+}
+
+#[inline(always)]
+pub fn get_texture_layout_info(
+    format: schema::TextureFormat,
+    width: u32,
+    height: u32,
+) -> TextureLayoutInfo {
+    use std::cmp::max;
+    let format_info = get_texture_format_info(format);
+    let width_by_block = max(1, width / format_info.block_width);
+    let height_by_block = max(1, height / format_info.block_height);
+    TextureLayoutInfo {
+        pitch: (width_by_block * format_info.block_bits) / 8,
+        slice_pitch: (width_by_block * height_by_block * format_info.block_bits) / 8,
     }
 }
